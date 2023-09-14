aa874d34f09b2ffcd3d8e90f197c2cbdf6d8a472
jio <giorgiosepe555@gmail.com>
2023-09-14 23:53:01 +0200

    little bit of error handling

--- openGLLearning/src/Main.cpp
+++ openGLLearning/src/Main.cpp
@@ -6,6 +6,19 @@
 #include <string>
 #include <sstream>
 
+static void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static void GLCheckError()
+{
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL Error] (" << error << ")" << std::endl;
+    }
+}
+
 struct ShaderProgramSource
 {
     std::string VertexSource;
