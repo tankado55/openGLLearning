0d223cdca4cd5fbfc0ce27f96dcecbb13e92251d
jio <mog955@gmail.com>
2024-01-14 20:07:21 +0100

    setting up a test framework (deleted things)

---
+++ openGLLearning/src/tests/TestClearColor.cpp
@@ -0,0 +1,31 @@
+#include "TestClearColor.h"
+
+#include "Renderer.h"
+#include "imgui.h"
+
+namespace Test {
+	TestClearColor::TestClearColor():
+		m_ClearColor{ 0.2f, 0.3f, 0.8f, 1.0f }
+	{
+	}
+
+	TestClearColor::~TestClearColor()
+	{
+	}
+
+	void TestClearColor::OnUpdate(float deltaTime)
+	{
+	}
+
+	void TestClearColor::OnRenderer()
+	{
+		GLCall(glClearColor(m_ClearColor[0], m_ClearColor[1], m_ClearColor[2], m_ClearColor[3]));
+		GLCall(glClear(GL_COLOR_BUFFER_BIT));
+	}
+
+	void TestClearColor::OnImGuiRenderer()
+	{
+		ImGui::ColorEdit4("Clear Color", m_ClearColor);
+	}
+}
+
---
+++ openGLLearning/src/tests/TestClearColor.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Test.h"
+
+namespace Test {
+
+	class TestClearColor : public Test
+	{
+	public:
+		TestClearColor();
+		~TestClearColor();
+
+		void OnUpdate(float deltaTime) override;
+		void OnRenderer() override;
+		void OnImGuiRenderer() override;
+
+	private:
+		float m_ClearColor[4];
+	};
+}
