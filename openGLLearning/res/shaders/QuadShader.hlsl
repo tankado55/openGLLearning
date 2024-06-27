fdfb286bed2c8790a37750540f7cb96fb23221f9
Giorgio <mog955@gmail.com>
2024-06-27 19:46:24 +0200

    small

--- openGLLearning/res/shaders/QuadShader.hlsl
+++ openGLLearning/res/shaders/QuadShader.hlsl
@@ -81,47 +81,6 @@ float shadowDensity = u_ShadowDensity * u_LigthStepSize;
 
 float extinctionCoefficient = u_AbsorptionCoefficient + u_ScatteringCoefficient;
 
-
-vec3 hash33(vec3 p3) {
-    vec3 p = fract(p3 * vec3(.1031, .11369, .13787));
-    p += dot(p, p.yxz + 19.19);
-    return -1.0 + 2.0 * fract(vec3((p.x + p.y) * p.z, (p.x + p.z) * p.y, (p.y + p.z) * p.x));
-}
-
-
-float worley(vec3 p, float scale) {
-
-    vec3 id = floor(p * scale);
-    vec3 fd = fract(p * scale);
-
-    float n = 0.;
-
-    float minimalDist = 1.;
-
-
-    for (float x = -1.; x <= 1.; x++) {
-        for (float y = -1.; y <= 1.; y++) {
-            for (float z = -1.; z <= 1.; z++) {
-
-                vec3 coord = vec3(x, y, z);
-                vec3 rId = hash33(mod(id + coord, scale)) * 0.5 + 0.5;
-
-                vec3 r = coord + rId - fd;
-
-                float d = dot(r, r);
-
-                if (d < minimalDist) {
-                    minimalDist = d;
-                }
-
-            }//z
-        }//y
-    }//x
-
-    return 1.0 - minimalDist;
-}
-
-
 int getVoxelIndex(vec3 pos)
 {
     vec4 localPoint = toVoxelLocal * vec4(pos, 1.0);
